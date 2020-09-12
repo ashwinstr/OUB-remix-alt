a2ba807bc912d9693b1d39f5ed67d7e1416784e1
ashwinstr <62101864+ashwinstr@users.noreply.github.com>
2020-09-12 12:53:47 +0530

    Update scrapers.py

--- userbot/modules/scrapers.py
+++ userbot/modules/scrapers.py
@@ -1153,7 +1153,7 @@ async def capture(url):
     driver = await chrome(chrome_options=chrome_options)
     prefix_str = 'http://'
     input_str = url.pattern_match.group(1)
-    complete_link = (({}{}).format(prefix_str, input_str))
+    complete_link = (("{}{}").format(prefix_str, input_str))
     link_match = match(r'\bhttps?://.*\.\S+', input_str)
     if link_match:
         link = link_match.group()
