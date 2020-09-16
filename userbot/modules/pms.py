0b6b417444b8dc06b854af09c90a6fcbe57776ce
ashwinstr <62101864+ashwinstr@users.noreply.github.com>
2020-09-16 19:07:18 +0530

    Update pms.py

--- userbot/modules/pms.py
+++ userbot/modules/pms.py
@@ -304,7 +304,7 @@ async def monito_p_m_s(event):
         if event.chat_id and NC_LOG_P_M_S:
                     await event.client.send_message(
                         PM_LOGGR_BOT_API_ID,
-                        "#Forwarded\n" + "User: " +
+                        "#Forwarded\n" + "From " +
                         f"[{chat.first_name}](tg://user?id={chat.id})",
                     )
 
