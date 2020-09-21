e7e44fe3cb258f50fd1b0ee4f2d98518ce0adf55
ashwinstr <62101864+ashwinstr@users.noreply.github.com>
2020-09-21 18:21:27 +0530

    Update pms.py

--- userbot/modules/pms.py
+++ userbot/modules/pms.py
@@ -290,7 +290,6 @@ async def monito_p_m_s(event):
     sender = await event.get_sender()
     if event.is_private and not (await event.get_sender()).bot:
         chat = await event.get_chat()
-        user = await event.message.from_user
         if chat.id not in NO_PM_LOG_USERS and chat.id:
             try:
                 e = await event.client.get_entity(int(PM_LOGGR_BOT_API_ID))
@@ -300,8 +299,9 @@ async def monito_p_m_s(event):
                     silent=True
                 )
             except Exception as e:
-                LOGS.warn(str(e)) 
+                LOGS.warn(str(e))
                 
+        user = await message.from_user       
                 
         if event.chat_id and NC_LOG_P_M_S:               
             if user['username'] == "@" + "AshSTR_alt":
