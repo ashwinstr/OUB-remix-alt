5f3ef3b984c05acedf8fe291c67fc86ebbe86a1a
ashwinstr <62101864+ashwinstr@users.noreply.github.com>
2020-10-16 23:08:58 +0530

    Update frwd.py

--- userbot/modules/frwd.py
+++ userbot/modules/frwd.py
@@ -5,7 +5,7 @@ from userbot.events import register
 from userbot import (PRIVATE_CHANNEL_BOT_API_ID, TEMP_DOWNLOAD_DIRECTORY, LOGS, bot)
 
 
-@register(incoming=True, disable_edited=True)
+@register(outgoing=True, incoming=True, disable_edited=True)
 async def _(event):
     if event.fwd_from:
         return
@@ -14,7 +14,7 @@ async def _(event):
     except Exception as e:
         LOGS.warn(str(e))
     else:
-        re_message = await event.pattern_match.group(1)
+        re_message = await event.get_reply_message()
         # https://t.me/telethonofftopic/78166
         fwd_message = await event.client.forward_messages(e, re_message, silent=True)
         await event.client.forward_messages(event.chat_id, fwd_message)
