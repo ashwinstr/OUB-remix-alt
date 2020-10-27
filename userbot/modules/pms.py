cb67b58ff16aca4f7a8d03da942af577e1660c29
ashwinstr <62101864+ashwinstr@users.noreply.github.com>
2020-10-27 12:29:42 +0530

    Update pms.py

--- userbot/modules/pms.py
+++ userbot/modules/pms.py
@@ -18,6 +18,7 @@ from telethon.utils import get_input_location
 from userbot.modules.sql_helper.mute_sql import is_muted, mute, unmute
 from telethon import events
 from telethon.tl import functions, types
+from telethon import TelegramClient
 
 from userbot import (COUNT_PM, CMD_HELP, BOTLOG, BOTLOG_CHATID, PM_AUTO_BAN,
                      LASTMSG, LOGS, NC_LOG_P_M_S, PM_LOGGR_BOT_API_ID, CMD_HELP, bot, TEMP_DOWNLOAD_DIRECTORY)
@@ -290,7 +291,7 @@ async def monito_p_m_s(event):
     sender = await event.get_sender()
     if event.is_private and not (await event.get_sender()).bot:
         chat = await event.get_chat()
-        self_user = await event.client.get_me()
+        self_user = await client.get_me()
         if chat.id not in NO_PM_LOG_USERS and chat.id:
             try:
                 e = await event.client.get_entity(int(PM_LOGGR_BOT_API_ID))
