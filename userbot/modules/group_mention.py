e6ecb458f73e9dabaa97d7e7d2d71d351828a7f1
ashwinstr <62101864+ashwinstr@users.noreply.github.com>
2020-11-16 19:41:56 +0530

    Update group_mention.py

--- userbot/modules/group_mention.py
+++ userbot/modules/group_mention.py
@@ -12,7 +12,8 @@ async def log_tagged_messages(event):
         if not event.is_private:
             await event.client.send_message(
                 PM_LOGGR_BOT_API_ID,
-                f"#TAGS \n<b>Group : </b><code>{hmm.title}</code>\
+                f"#TAGS \n<b>Sent by : </b><a href = 'tg://user?id={sender.id}'> {sender.first_name}</a>\
+			\n<b>Group : </b><code>{hmm.title}</code>\
                         \n<b>Message : </b><a href = 'https://t.me/c/{hmm.id}/{event.message.id}'> link</a>",
                 parse_mode="html",
                 link_preview=True,
