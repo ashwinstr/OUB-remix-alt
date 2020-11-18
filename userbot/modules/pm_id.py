08513f595881924a015b61ab2c360796537d4e92
ashwinstr <62101864+ashwinstr@users.noreply.github.com>
2020-11-18 10:17:13 +0530

    Update pm_id.py

--- userbot/modules/pm_id.py
+++ userbot/modules/pm_id.py
@@ -34,6 +34,6 @@ async def monito_p_m_s(event):
                     COUNT = 0
                 await event.client.send_message(
                     PM_LOGGR_BOT_API_ID,
-                    f"[{sender.first_name}(tg://user?id={sender.id}) has sent a new message \nId : `{chat.id}`",
+                    f"[{sender.first_name}](tg://user?id={sender.id}) has sent a new message \n**Id : **`{chat.id}`",
                 )
                 COUNT += 1
