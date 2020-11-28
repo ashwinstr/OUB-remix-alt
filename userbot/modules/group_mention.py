8448cf6363bc2298486082cfb1f057d1f154c9fd
ashwinstr <62101864+ashwinstr@users.noreply.github.com>
2020-11-28 16:44:37 +0530

    Update group_mention.py

--- userbot/modules/group_mention.py
+++ userbot/modules/group_mention.py
@@ -19,16 +19,17 @@ async def log_tagged_messages(event):
                 link_preview=True,
             )
         else:
-            await event.client.send_message(
-                PM_LOGGR_BOT_API_ID,
-                f"#TAGS \n<b>Sent by : </b><a href = 'tg://user?id={sender.id}'> {sender.first_name}</a>\
-                        \n<b>ID : </b><code>{sender.id}</code>",
-                parse_mode="html",
-                link_preview=True,
-            )
-        e = await event.client.get_entity(int(PM_LOGGR_BOT_API_ID))
-        fwd_message = await event.client.forward_messages(
-                    e,
-                    event.message,
-                    silent=True
+            if not (await event.get_sender()).bot:
+                await event.client.send_message(
+                    PM_LOGGR_BOT_API_ID,
+                    f"#TAGS \n<b>Sent by : </b><a href = 'tg://user?id={sender.id}'> {sender.first_name}</a>\
+                            \n<b>ID : </b><code>{sender.id}</code>",
+                    parse_mode="html",
+                    link_preview=True,
                 )
+                e = await event.client.get_entity(int(PM_LOGGR_BOT_API_ID))
+                fwd_message = await event.client.forward_messages(
+                        e,
+                        event.message,
+                        silent=True
+                    )
