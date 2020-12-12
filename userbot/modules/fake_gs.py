43074be6e23b27343ef463bb11b98ab979334f55
ashwinstr <62101864+ashwinstr@users.noreply.github.com>
2020-12-12 17:01:12 +0530

    Update fake_gs.py

--- userbot/modules/fake_gs.py
+++ userbot/modules/fake_gs.py
@@ -1,14 +1,3 @@
-<<<<<<< HEAD
-import asyncio
-import os 
-from PIL import Image, ImageDraw, ImageFont
-from telethon.tl.types import DocumentAttributeFilename
-from wget import download
-
-from userbot import CMD_HELP, bot
-from userbot.events import register
-from userbot.utils import googleimagesdownload
-=======
 """Get a user-customised google search meme!"""
 
 # Plugin By - XlayerCharon[XCB]
@@ -21,7 +10,7 @@ from PIL import Image, ImageDraw, ImageFont
 from wget import download
 
 from userbot.events import register
->>>>>>> b8cd5ed2e1356b6f5af92f98e8e8fdea8a6e1a48
+
 
 @register(outgoing=True, pattern="^.fgs ((.*) ; (.*))")
 async def FakeGoogleSearch(event):
@@ -44,13 +33,8 @@ async def FakeGoogleSearch(event):
     drawing=ImageDraw.Draw(photo)
     blue=(0,0,255)
     black=(0,0,0)
-<<<<<<< HEAD
-    font1=ImageFont.truetype("resources/ProductSans-BoldItalic.ttf",20)
-    font2=ImageFont.truetype("resources/ProductSans-Light.ttf",23)
-=======
     font1=ImageFont.truetype("userbot/utils/styles/ProductSans-BoldItalic.ttf",20)
     font2=ImageFont.truetype("userbot/utils/styles/ProductSans-Light.ttf",23)
->>>>>>> b8cd5ed2e1356b6f5af92f98e8e8fdea8a6e1a48
     drawing.text((450, 258), result, fill=blue, font=font1)
     drawing.text((270, 37), search, fill=black, font=font2)
     photo.save("downloads/test.jpg")
@@ -58,16 +42,11 @@ async def FakeGoogleSearch(event):
     await event.delete()
     reply_id = event.pattern_match.group(3) if reply else None
     await event.client.send_file(
-<<<<<<< HEAD
         event.chat.id,
-=======
-        event.chat_id,
->>>>>>> b8cd5ed2e1356b6f5af92f98e8e8fdea8a6e1a48
         'downloads/test.jpg',
         reply_to_message_id=reply_id)
     os.remove('downloads/test.jpg')
 
-<<<<<<< HEAD
     CMD_HELP.update(
     {
         "fake_google_search": "`.fgs`\n"
@@ -75,5 +54,3 @@ async def FakeGoogleSearch(event):
         "`.fgs [UpperText] ; [LowerText]`\n"
     }
 )
-=======
->>>>>>> b8cd5ed2e1356b6f5af92f98e8e8fdea8a6e1a48
